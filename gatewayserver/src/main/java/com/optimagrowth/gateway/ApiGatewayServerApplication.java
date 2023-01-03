9ab33ab349fcadcda696a081c6fb5c5c97ba7bcb
Starostenko Ruslan <starslan-1@yandex.ru>
2023-01-04 02:20:03 +0300

    eureka and gateway

--- gatewayserver/src/main/java/com/optimagrowth/gateway/ApiGatewayServerApplication.java
+++ gatewayserver/src/main/java/com/optimagrowth/gateway/ApiGatewayServerApplication.java
@@ -2,10 +2,14 @@ package com.optimagrowth.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@RefreshScope
 public class ApiGatewayServerApplication {
 
 	public static void main(String[] args) {
