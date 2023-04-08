95849c98fb01f4fa1429b27dd2c5f30b4f71b11d
Starostenko Ruslan <starslan-1@yandex.ru>
2023-04-08 22:24:13 +0300

    kafka

--- organization-service/src/main/java/com/optimagrowth/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/com/optimagrowth/organizationservice/OrganizationServiceApplication.java
@@ -1,11 +1,18 @@
 package com.optimagrowth.organizationservice;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+
 
 @SpringBootApplication
 @EnableEurekaClient
+@RefreshScope
+@EnableBinding(Source.class)
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
--- organization-service/src/main/java/com/optimagrowth/organizationservice/service/OrganizationService.java
+++ organization-service/src/main/java/com/optimagrowth/organizationservice/service/OrganizationService.java
@@ -1,5 +1,6 @@
 package com.optimagrowth.organizationservice.service;
 
+import com.optimagrowth.organizationservice.events.source.SimpleSourceBean;
 import com.optimagrowth.organizationservice.model.Organization;
 import com.optimagrowth.organizationservice.repository.OrganizationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,9 @@ public class OrganizationService {
     @Autowired
     private OrganizationRepository repository;
 
+    @Autowired
+    SimpleSourceBean simpleSourceBean;
+
     public Organization findById(String organizationId) {
         Optional<Organization> opt = repository.findById(organizationId);
         return opt.orElse(null);
@@ -22,16 +26,20 @@ public class OrganizationService {
     public Organization create(Organization organization){
         organization.setId( UUID.randomUUID().toString());
         organization = repository.save(organization);
+        simpleSourceBean.publishOrganizationChange("CREATED", organization.getId());
         return organization;
 
     }
 
     public void update(Organization organization){
         repository.save(organization);
+        simpleSourceBean.publishOrganizationChange("UPDATED", organization.getId());
+
     }
 
     public void delete(String organizationId){
         repository.deleteById(organizationId);
+        simpleSourceBean.publishOrganizationChange("DELETED", organizationId);
     }
 
 }
